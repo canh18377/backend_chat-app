ef65332cfc6ca4cf2959e50dfa8f04462a12fecd
canh18377 <canh10101101@gmail.com>
2025-04-10 08:19:52 +0700

    D

--- src/routes/authRoute/open-auth.js
+++ src/routes/authRoute/open-auth.js
@@ -4,7 +4,11 @@ const passport = require('passport');
 const router = express.Router();
 const authController = require('../../controllers/authController')
 router.get('/facebook', authController.authFB);
-router.get('/google', authController.authGG);
+router.get('/google', (req, res) => {
+    console.log("auth google")
+    res.next()
+})
+    , authController.authGG;
 router.get('/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/', session: false }), authController.responseAfterAuth);
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/', session: false }), authController.responseAfterAuth);
 module.exports = router;
