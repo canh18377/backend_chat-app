e3d5195d9462924873322ff757dd036e080f2f42
canh18377 <154729960+canh18377@users.noreply.github.com>
2025-04-12 10:35:13 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ config_websoket(server)
 app.use('/uploads', express.static(path.join(__dirname, "src", 'uploads')));
 app.use(passport.initialize());
 authRoute(app);
-app.use(verifyAccessToken)
+// app.use(verifyAccessToken)
 private_route(app)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
