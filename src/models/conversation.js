a6829efed41082bca0ad4156628d066672f6046a
canh18377 <canh10101101@gmail.com>
2025-04-12 10:29:44 +0700

    c

--- src/models/conversation.js
+++ src/models/conversation.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ConversationSchema = new mongoose.Schema({
     participants: [
         {
-            type: mongoose.Schema.Types.ObjectId,
+            type: String,
             ref: 'User'
         }
     ],
