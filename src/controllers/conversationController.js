8c20ec05b88636e64ec1956a3603f98d2d54bb3b
canh18377 <canh10101101@gmail.com>
2025-04-15 15:10:52 +0700

    D

--- src/controllers/conversationController.js
+++ src/controllers/conversationController.js
@@ -22,6 +22,7 @@ class conversationController {
                 } else {
                     const receiverId = conv.participants.find(id => id !== req.user.idUser);
                     const user = await User.findOne({ idUser: receiverId });
+                    delete user.password;
                     results.push({ conversation: conv, lastMessage, user });
                 }
             }
