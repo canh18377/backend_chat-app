568b86973fc121909b562bfdf6466c8a0018114e
canh18377 <canh10101101@gmail.com>
2025-04-15 08:10:04 +0700

    D

--- src/routes/privateRoute/index.js
+++ src/routes/privateRoute/index.js
@@ -1,7 +1,9 @@
 const userRoute = require('./userRoute');
 const conversationRoute = require('./conversationRoute');
+const messageRoute = require('./messageRoute');
 function private_route(app) {
     app.use("/api/user", userRoute);
     app.use("/api/conversations", conversationRoute);
+    app.use("/api/message", messageRoute);
 }
 module.exports = private_route;
