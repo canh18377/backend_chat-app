25c47bcd8b72f202e796f299447693f97046ff7f
canh18377 <canh10101101@gmail.com>
2025-04-15 15:19:56 +0700

    D

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -4,30 +4,23 @@ const Conversation = require('../models/conversation'); // Model Conversation
 class message {
     async getMessage(req, res) {
         try {
-            // Lấy conversationId từ request
-            const { conversationId } = req.params; // Sử dụng req.params nếu nó được truyền qua URL, hoặc req.body nếu từ body
-            console.log(conversationId)
-            console.log(222)
-            // Kiểm tra xem có conversationId không
+            const { conversationId } = req.params;
             if (!conversationId) {
                 return res.status(400).json({ message: "Conversation ID is required" });
             }
 
-            // Truy vấn tất cả tin nhắn liên quan đến conversationId
-            const messages = await Message.find({ conversationId: conversationId })
-                .sort({ timestamp: 1 }) // Sắp xếp theo thứ tự thời gian
-            console.log(messages)
-            // Kiểm tra xem có tin nhắn nào không
+            const messages = await Message.find({ conversationId: conversationId.trim() }).sort({ timestamp: 1 }).lean();
+
             if (!messages || messages.length === 0) {
                 return res.json([]);
             }
-            // Trả về danh sách tin nhắn
             return res.status(200).json({ messages });
         } catch (error) {
             console.error("Error fetching messages:", error);
             return res.status(500).json({ message: "Internal server error" });
         }
     }
+
 
     createMessage = async (senderId, receiverId, message, isGroup = false, groupName = '', groupAvatar = '') => {
         try {
