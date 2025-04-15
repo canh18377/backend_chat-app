4ee7bc80dd7c8e8998d96fcefde1bbaf68c47c08
canh18377 <canh10101101@gmail.com>
2025-04-15 17:02:16 +0700

    D

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,6 +1,6 @@
 const Message = require('../models/message'); // Đảm bảo đã có model Message
 const Conversation = require('../models/conversation'); // Model Conversation
-const mongoose = require("mongoose");
+
 class message {
     async getMessage(req, res) {
         try {
@@ -8,23 +8,16 @@ class message {
             if (!conversationId) {
                 return res.status(400).json({ message: "Conversation ID is required" });
             }
-            const conversationObjectId = new mongoose.Types.ObjectId(conversationId.trim());
-            console.log(conversationObjectId)
-            const msa = await Message.find()
-            console.log(msa)
-            console.log(msa[0].conversationId === conversationObjectId)
-            const messages = await Message.find({ conversationId: conversationObjectId }).sort({ timestamp: 1 }).lean();
 
-            if (!messages || messages.length === 0) {
-                return res.json([]);
-            }
+            const messages = await Message.find({ conversationId: conversationId })
+                .sort({ timestamp: 1 }).lean()// Sắp xếp theo thứ tự thời gian
+            console.log(messages)
             return res.status(200).json({ messages });
         } catch (error) {
             console.error("Error fetching messages:", error);
             return res.status(500).json({ message: "Internal server error" });
         }
     }
-
 
     createMessage = async (senderId, receiverId, message, isGroup = false, groupName = '', groupAvatar = '') => {
         try {
