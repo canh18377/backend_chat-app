70385581f9a3c7029887ebb7cc37553409811481
canh18377 <canh10101101@gmail.com>
2025-04-18 19:26:08 +0700

    APi friend ship

--- src/routes/privateRoute/friendRoute.js
+++ src/routes/privateRoute/friendRoute.js
@@ -5,7 +5,7 @@ const friendshipController = require('../../controllers/friendshipController');
 router.post('/send-request', friendshipController.sendRequest);
 router.put('/accept/:requestId', friendshipController.acceptRequest);
 router.delete('/reject/:requestId', friendshipController.rejectRequest);
-router.get('/friends', friendshipController.getFriends);
+// router.get('/friends', friendshipController.getFriends);
 router.get('/requests/received', friendshipController.getListFriendRequest);
 router.get('/requests/sent', friendshipController.getSentFriendRequests);
 module.exports = router;
