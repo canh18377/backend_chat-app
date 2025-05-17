8fb69dbb3ddac1b16dcc843e3b2de29f34490ba9
canh18377 <canh10101101@gmail.com>
2025-05-17 23:51:22 +0700

    S

--- src/routes/privateRoute/userRoute.js
+++ src/routes/privateRoute/userRoute.js
@@ -6,7 +6,11 @@ const upload = require("../../middlewares/multer")
 // router.post('/login', userController.login);
 router.get('/me', userController.getCurrentUser);
 router.get('/getUsers/:searchBy', userController.getUser);
-router.put('/update', upload.fields([{ name: 'avatar', maxCount: 1 }], userController.updateUser));
+router.put(
+    '/update',
+    upload.fields([{ name: 'avatar', maxCount: 1 }]),
+    userController.updateUser
+);
 router.delete('/delete', userController.deleteUser);
 
 module.exports = router;
