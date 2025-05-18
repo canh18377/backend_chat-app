7078361fdb6c49530e434b731c8fc79df1a87bb2
canh18377 <canh10101101@gmail.com>
2025-05-18 12:04:48 +0700

    S

--- src/routes/privateRoute/messageRoute.js
+++ src/routes/privateRoute/messageRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("../../middlewares/multer")
 const messageController = require('../../controllers/messageController');
 router.get('/:exist/:data', messageController.getMessage);
 router.post(
