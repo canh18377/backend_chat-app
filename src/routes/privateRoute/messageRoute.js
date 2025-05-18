c155a1cbe69fba6a60ace25ed6b1ecb3383a6e8b
canh18377 <canh10101101@gmail.com>
2025-05-18 12:01:13 +0700

    S

--- src/routes/privateRoute/messageRoute.js
+++ src/routes/privateRoute/messageRoute.js
@@ -2,4 +2,9 @@ const express = require("express");
 const router = express.Router();
 const messageController = require('../../controllers/messageController');
 router.get('/:exist/:data', messageController.getMessage);
+router.post(
+    '/saveImg',
+    upload.single("image"),
+    messageController.saveImage
+);
 module.exports = router;
