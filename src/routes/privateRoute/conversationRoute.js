6a9ffe395ee36fcf311967c39cf21e0a13460a48
QuocDuy21-10 <quocduy37na@gmail.com>
2025-05-26 10:42:25 +0700

    add route create group

--- src/routes/privateRoute/conversationRoute.js
+++ src/routes/privateRoute/conversationRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const conversationController = require('../../controllers/conversationController');
+const upload = require("../../middlewares/multer")
 router.get('/', conversationController.getConversations);
 
+router.post('/create-group', upload.single('avatar'), conversationController.createConversation);
 
 module.exports = router;
